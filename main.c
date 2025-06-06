ce24128ab3aaead47e1b06aae6ad88890b57d1dd
Diego Carrillo <dicarrillo04@gmail.com>
2025-06-06 02:25:50 -0600

    Implement half adder function
    A function that simulates a half adder using
    the previously-implemented logic gate functions has been
    added, along with a corresponding test function, similar to
    the logic gate test functions.

--- main.c
+++ main.c
@@ -5,9 +5,10 @@ void test_not_gate();
 void test_and_gate();
 void test_or_gate();
 void test_xor_gate();
+void test_half_adder();
 
 // Simulate a NOT gate
-// Input value must be 0 or 1
+// Input: bit_a must be 0 or 1
 int not_gate(int bit_a) {
     if (bit_a == 0) {
         return 1;
@@ -18,7 +19,7 @@ int not_gate(int bit_a) {
 }
 
 // Simulate an AND gate
-// Input values must be 0 or 1
+// Inputs: bit_a and bit_b must be 0 or 1
 int and_gate(int bit_a, int bit_b) {
     if ((bit_a == 1) && (bit_b == 1)) {
         return 1;
@@ -29,7 +30,7 @@ int and_gate(int bit_a, int bit_b) {
 }
 
 // Simulate an OR gate
-// Input values must be 0 or 1
+// Inputs: bit_a and bit_b must be 0 or 1
 int or_gate(int bit_a, int bit_b) {
     if ((bit_a == 1) || (bit_b == 1)) {
         return 1;
@@ -40,7 +41,7 @@ int or_gate(int bit_a, int bit_b) {
 }
 
 // Simulate an XOR gate
-// Input values must be 0 or 1
+// Inputs: bit_a and bit_b must be 0 or 1
 int xor_gate(int bit_a, int bit_b) {
     int not_a;
     int not_b;
@@ -56,12 +57,23 @@ int xor_gate(int bit_a, int bit_b) {
     return or_gate(and_result_1, and_result_2);
 }
 
+// Simulate a half adder
+// Inputs: bit_a and bit_b must be 0 or 1
+// Outputs: sum and carry are pointers to ints that store the sum and carry values
+void half_adder(int bit_a, int bit_b, int* sum, int* carry) {
+    *sum = xor_gate(bit_a, bit_b);
+    *carry = and_gate(bit_a, bit_b);
+}
+
 int main() {
     // Test logic gate outputs
     test_not_gate();
     test_and_gate();
     test_or_gate();
     test_xor_gate();
+    
+    // Test half adder
+    test_half_adder();
 
     return 0;
 }
@@ -131,6 +143,28 @@ void test_xor_gate() {
             bit_b = b;
             result = xor_gate(bit_a, bit_b);
             printf("Result of %d XOR %d: %d\n", bit_a, bit_b, result);
+        }
+    }
+
+    printf("\n");
+}
+
+// Test half adder for all values of bits 'a' and 'b'
+void test_half_adder() {
+    int bit_a;
+    int bit_b;
+    int sum;
+    int carry;
+
+    for (int a = 0; a <= 1; ++a) {
+        bit_a = a;
+
+        for (int b = 0; b <= 1; ++b) {
+            bit_b = b;
+            half_adder(bit_a, bit_b, &sum, &carry);
+
+            printf("Result of adding %d and %d...\n", bit_a, bit_b);
+            printf("Sum: %d\nCarry: %d\n", sum, carry);
         }
     }
 
