f4fb9e8b148b445676fe196edce8c377a13d0fbd
Diego Carrillo <dicarrillo04@gmail.com>
2025-06-06 01:36:18 -0600

    Create functions to test logic gates
    A test function has been created for each implemented
    logic gate, printing the result of each possible combination
    of inputs to the gate.

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+// Function prototypes
+void test_not_gate();
+void test_and_gate();
+void test_or_gate();
+
 // Simulates a NOT gate
 int not_gate(int bit_a) {
     if (bit_a == 0) {
@@ -30,14 +35,62 @@ int or_gate(int bit_a, int bit_b) {
     }
 }
 
-int main()
-{
+int main() {
+    test_not_gate();
+    test_and_gate();
+    test_or_gate();
+
+    return 0;
+}
+
+// Tests NOT gate for all values of bit 'a'
+void test_not_gate() {
+    int bit_a;
+    int result;
+
+    for (int a = 0; a <= 1; ++a) {
+        bit_a = a;
+        result = not_gate(bit_a);
+        printf("Result of NOT %d: %d\n", bit_a, result);
+    }
+
+    printf("\n");
+}
+
+// Tests AND gate for all values of bits 'a' and 'b'
+void test_and_gate() {
     int bit_a;
     int bit_b;
     int result;
 
-    bit_a = 0;
-    bit_b = 0;
+    for (int a = 0; a <= 1; ++a) {
+        bit_a = a;
 
-    return 0;
+        for (int b = 0; b <= 1; ++b) {
+            bit_b = b;
+            result = and_gate(bit_a, bit_b);
+            printf("Result of %d AND %d: %d\n", bit_a, bit_b, result);
+        }
+    }
+
+    printf("\n");
+}
+
+// Test OR gate for all values of bits 'a' and 'b'
+void test_or_gate() {
+    int bit_a;
+    int bit_b;
+    int result;
+
+    for (int a = 0; a <= 1; ++a) {
+        bit_a = a;
+
+        for (int b = 0; b <= 1; ++b) {
+            bit_b = b;
+            result = or_gate(bit_a, bit_b);
+            printf("Result of %d OR %d: %d\n", bit_a, bit_b, result);
+        }
+    }
+
+    printf("\n");
 }
