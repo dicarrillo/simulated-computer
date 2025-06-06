5cf1ef335a0e90fa394c41e3f702996af1e1dadb
Diego Carrillo <dicarrillo04@gmail.com>
2025-06-06 02:54:22 -0600

    Implement full adder function
    A function that simulates a full adder using the
    previously-implemented logic gate functions has been added,
    along with a corresponding test function, similar to the
    logic gate and half adder test functions.

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ void test_and_gate();
 void test_or_gate();
 void test_xor_gate();
 void test_half_adder();
+void test_full_adder();
 
 // Simulate a NOT gate
 // Input: bit_a must be 0 or 1
@@ -45,24 +46,40 @@ int or_gate(int bit_a, int bit_b) {
 int xor_gate(int bit_a, int bit_b) {
     int not_a;
     int not_b;
-    int and_result_1;
-    int and_result_2;
+    int a_and_not_b;
+    int not_a_and_b;
 
     not_a = not_gate(bit_a);
     not_b = not_gate(bit_b);
 
-    and_result_1 = and_gate(bit_a, not_b);
-    and_result_2 = and_gate(not_a, bit_b);
+    a_and_not_b = and_gate(bit_a, not_b);
+    not_a_and_b = and_gate(not_a, bit_b);
 
-    return or_gate(and_result_1, and_result_2);
+    return or_gate(a_and_not_b, not_a_and_b);
 }
 
 // Simulate a half adder
 // Inputs: bit_a and bit_b must be 0 or 1
-// Outputs: sum and carry are pointers to ints that store the sum and carry values
+// Outputs: sum and carry_out are pointers to ints that store the sum and carry out values
 void half_adder(int bit_a, int bit_b, int* sum, int* carry_out) {
     *sum = xor_gate(bit_a, bit_b);
     *carry_out = and_gate(bit_a, bit_b);
+}
+
+// Simulate a full adder
+// Inputs: bit_a and bit_b must be 0 or 1
+// Outputs: sum and carry_out are pointers to ints that store the sum and carry out values
+void full_adder(int bit_a, int bit_b, int carry_in, int* sum, int* carry_out)
+{
+    int a_xor_b;
+    int a_xor_b_and_c;
+    int a_and_b;
+
+    a_xor_b = xor_gate(bit_a, bit_b);
+    *sum = xor_gate(a_xor_b, carry_in);
+    a_xor_b_and_c = and_gate(a_xor_b, carry_in);
+    a_and_b = and_gate(bit_a, bit_b);
+    *carry_out = or_gate(a_xor_b_and_c, a_and_b);
 }
 
 int main() {
@@ -72,8 +89,9 @@ int main() {
     test_or_gate();
     test_xor_gate();
     
-    // Test half adder
+    // Test adder outputs
     test_half_adder();
+    test_full_adder();
 
     return 0;
 }
@@ -165,6 +183,33 @@ void test_half_adder() {
 
             printf("Result of adding %d and %d...\n", bit_a, bit_b);
             printf("Sum: %d\nCarry Out: %d\n", sum, carry_out);
+        }
+    }
+
+    printf("\n");
+}
+
+// Test full adder for all values of bits 'a' and 'b', and carry in bit
+void test_full_adder() {
+    int bit_a;
+    int bit_b;
+    int carry_in;
+    int sum;
+    int carry_out;
+
+    for (int a = 0; a <= 1; ++a) {
+        bit_a = a;
+
+        for (int b = 0; b <= 1; ++b) {
+            bit_b = b;
+
+            for (int c = 0; c <= 1; ++c) {
+                carry_in = c;
+                full_adder(bit_a, bit_b, carry_in, &sum, &carry_out);
+
+                printf("Result of adding %d, %d, and %d...\n", bit_a, bit_b, carry_in);
+                printf("Sum: %d\nCarry Out: %d\n", sum, carry_out);
+            }
         }
     }
 
